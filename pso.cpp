d66ce417ad57a86b401c3ae9af8b787711561f6a
Changjie <changjieguan@gmail.com>
2018-04-21 14:17:42 +0200

    Test rng

---
+++ pso.cpp
@@ -0,0 +1,12 @@
+//
+// Copyright 2018 TASS International.
+//
+// All rights reserved.
+//
+// Author: Changjie Guan (changjie.guan@tassinternational.com)
+//
+#include <stdio.h>
+#include <math.h>
+
+#include "pso.hpp"
+
---
+++ test_pso.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+#include "pso.hpp"
+
+
+int main()
+{
+	MPC_PSO_Solver solver;
+
+	//solver.getRNG();
+	//solver.printRNG();
+
+	//solver.getRNG();
+	//solver.printRNG();
+	return 0;
+}
